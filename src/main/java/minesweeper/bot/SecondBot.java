920f52ad508398522cef030fb425c69b60f0f7a5
Julia Bergman <julia.bergman@helsinki.fi>
2021-02-04 00:04:46 +0200

    AMN and AFN work but are not tested

--- src/main/java/minesweeper/bot/SecondBot.java
+++ src/main/java/minesweeper/bot/SecondBot.java
@@ -3,8 +3,14 @@ package minesweeper.bot;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import minesweeper.model.Board;
 import minesweeper.model.GameStats;
 import minesweeper.model.Move;
@@ -18,11 +24,15 @@ public class SecondBot implements Bot {
     private Random rng = new Random();
     private GameStats gameStats;
     private boolean firstMove = true;
-    private Set<Square> setS = new HashSet<>();
-    private Set<Square> setQ = new HashSet<>();
+    private Set<Square> setS = new LinkedHashSet<>();
+    private Set<Square> setQ = new LinkedHashSet<>();
     private Set<Square> marked = new HashSet<>();
+    private Set<Square> opened = new HashSet<>();
+    private ArrayList<Square> flagsToDraw = new ArrayList<>();
     private Square prev; //most recently opened square
-
+    private boolean makeRandomMoves = false;
+    
+    
     /**
      * Make a single decision based on the given Board state
      *
@@ -31,57 +41,90 @@ public class SecondBot implements Bot {
      */
     @Override
     public Move makeMove(Board board) {
+        if (!flagsToDraw.isEmpty()) {
+            Square s = flagsToDraw.get(0);
+            flagsToDraw.remove(0);
+            return new Move(MoveType.FLAG, s.getX(), s.getY());
+        }
+        if (makeRandomMoves) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                
+            }
+            return new Move(MoveType.FLAG, board.width - 1, board.height - 1);
+        }
+        System.out.println("makeMove called: S = "
+                + squareSetToString(setS) + "\n Q = "
+                + squareSetToString(setQ) + "\n marked = "
+                + squareSetToString(marked) + "\n prev = "
+                + squareToString(prev));
+
         if (firstMove) {
             firstMove = false;
             prev = board.getSquareAt(0, 0);
             System.out.println("First move");
-            return new Move(MoveType.OPEN, 0, 0);
+            return openMove(prev);
         }
 
         if (isAFN(prev, board)) {
+            System.out.println("isAFN = true");
             setS.addAll(getUnmarkedNeighbours(prev, board));
         } else {
+            System.out.println("isAFN = false, prev added to setQ");
             setQ.add(prev);
         }
 
-        if (!setS.isEmpty()) {
-            prev = setS.iterator().next();
-            setS.remove(prev);
-            System.out.println("move from S");
-            return new Move(MoveType.OPEN, prev.getX(), prev.getY());
-        }
+        while (true) {
+            if (!setS.isEmpty()) {
+                prev = setS.iterator().next();
+                setS.remove(prev);
+                System.out.println("setS is not empty returning "
+                        + squareToString(prev));
+                return openMove(prev);
+            }
 
-        Iterator<Square> it = setQ.iterator();
-        while (it.hasNext()) {
-            Square q = it.next();
-            if (isAMN(q, board)) {
-                for (Square y : getUnmarkedNeighbours(q, board)) {
-                    marked.add(y);
+            {
+                Iterator<Square> it = setQ.iterator();
+                while (it.hasNext()) {
+                    Square q = it.next();
+                    System.out.println("Checking isAMN(" + squareToString(q) + ")");
+                    if (isAMN(q, board)) {
+                        System.out.println("isAMN = true");
+                        for (Square y : getUnmarkedNeighbours(q, board)) {
+                            System.out.println("marked.add(" + squareToString(y) + ")");
+                            marked.add(y);
+                            flagsToDraw.add(y);
+                        }
+                        it.remove();
+                    }
                 }
-                setQ.remove(q);
+            }
+            {
+                Iterator<Square> it = setQ.iterator();
+                while (it.hasNext()) {
+                    Square q = it.next();
+                    System.out.println("Checking isAFN(" + squareToString(q) + ")");
+                    if (isAFN(q, board)) {
+                        System.out.println("isAFN = true");
+                        setS.addAll(getUnmarkedNeighbours(q, board));
+                        it.remove();
+                    }
+                }
+            }
+
+            if (setS.isEmpty()) {
+                //Here should be the third algorithm CSP? Now just random square
+                System.out.println("Picking random (S is empty)");
+                Pair<Integer> pair = findUnopenedSquare(board);
+                int x = (int) pair.first;
+                int y = (int) pair.second;
+                makeRandomMoves = true;
+                return new Move(MoveType.FLAG, board.width - 1, board.height - 1);
+
+                //setS.add(board.getSquareAt(x, y));
             }
         }
-
-        for (Square q : setQ) {
-            if (isAFN(q, board)) {
-                setS.addAll(getUnmarkedNeighbours(q, board));
-                setQ.remove(q);
-            }
-        }
-
-        if (setS.isEmpty()) {
-            //Here should be the third algorithm CSP? Now just random square
-            System.out.println("Picking random (S is empty)");
-            Pair<Integer> pair = findUnopenedSquare(board);
-            int x = (int) pair.first;
-            int y = (int) pair.second;
-
-            prev = board.getSquareAt(x, y);
-            return new Move(MoveType.OPEN, x, y);
-        }
-        
-        System.out.println("Calling makeMove from MakeMove");
-        return makeMove(board);
     }
 
     /**
@@ -102,18 +145,16 @@ public class SecondBot implements Bot {
     }
 
     /**
-     * Determine if an opened square fills in the AFN terms.
-     * In the case of AFN (All free neighbours) the square has as many
-     * flagged neighbours as it's number. So all other neighbours except
-     * the flagged ones are safe to open.
-     * 
+     * Determine if an opened square fills in the AFN terms. In the case of AFN
+     * (All free neighbours) the square has as many flagged neighbours as it's
+     * number. So all other neighbours except the flagged ones are safe to open.
+     *
      * @param square the square which neighbours are examined.
      * @param board The current board state.
      * @return if square is an AFN case.
      */
     private boolean isAFN(Square square, Board board) {
         int flaggedNeighbours = 0;
-        int unopenedNeighbours = 0;
 
         for (int xInc = -1; xInc <= 1; xInc++) {
             for (int yInc = -1; yInc <= 1; yInc++) {
@@ -122,42 +163,42 @@ public class SecondBot implements Bot {
                 }
                 if (board.withinBoard(square.getX() + xInc, square.getY() + yInc)) {
                     Square s = board.getSquareAt(square.getX() + xInc, square.getY() + yInc);
-                    if (!s.isOpened() && isFlagged(s)) {
+                    if (!s.isOpened() && isMarked(s)) {
                         flaggedNeighbours++;
-                    } else if (!s.isOpened()) {
-                        unopenedNeighbours++;
                     }
                 }
             }
         }
-        return square.surroundingMines() == flaggedNeighbours
-                && unopenedNeighbours > 0;
+        System.out.println("isAFN(" + squareToString(square) + ") = " + flaggedNeighbours);
+        return square.surroundingMines() == flaggedNeighbours;
     }
 
     /**
      * /**
-     * Determine if an opened square fills in the AMN terms.
-     * In the case of AMN (All mine/marked neighbours) the square has as many
-     * unopened neighbours as it's number. So all of it's neighbours are mines.
-     * 
+     * Determine if an opened square fills in the AMN terms. In the case of AMN
+     * (All mine/marked neighbours) the square has as many unopened neighbours
+     * as it's number. So all of it's neighbours are mines.
+     *
      * @param square the square which neighbours are examined.
      * @param board the current board state.
      * @return if square is an AMN case.
      */
     private boolean isAMN(Square square, Board board) {
-        Set<Square> unmarkedNeighbours = getUnmarkedNeighbours(square, board);
-        return square.surroundingMines() == unmarkedNeighbours.size();
+        Set<Square> markedNeighbours = getMarkedNeighbours(square, board);
+        System.out.println("AMN:markedNeighbours = " + markedNeighbours.size());
+        return square.surroundingMines() - markedNeighbours.size() == 
+                getUnmarkedNeighbours(square, board).size();
     }
 
     /**
      * Get the square's all unopened unmarked neighbours as a set.
-     * 
+     *
      * @param square the square which neighbours are examined
      * @param board the current board state
-     * @return 
+     * @return set containing the square's unopened unmarked neighbours
      */
     private Set<Square> getUnmarkedNeighbours(Square square, Board board) {
-        Set<Square> neighbours = new HashSet<>();
+        Set<Square> neighbours = new LinkedHashSet<>();
 
         for (int xInc = -1; xInc <= 1; xInc++) {
             for (int yInc = -1; yInc <= 1; yInc++) {
@@ -168,7 +209,7 @@ public class SecondBot implements Bot {
                 int y = square.getY() + yInc;
                 if (board.withinBoard(square.getX() + xInc, square.getY() + yInc)) {
                     Square s = board.getSquareAt(x, y);
-                    if (!s.isOpened() && !isFlagged(s)) {
+                    if (!opened.contains(s) && !isMarked(s)) {
                         neighbours.add(s);
                     }
                 }
@@ -176,14 +217,41 @@ public class SecondBot implements Bot {
         }
         return neighbours;
     }
-    
+    /**
+     * Get the square's all marked neighbours as a set.
+     *
+     * @param square the square which neighbours are examined
+     * @param board the current board state
+     * @return set containing the square's marked neighbours
+     */
+    private Set<Square> getMarkedNeighbours(Square square, Board board) {
+        Set<Square> neighbours = new LinkedHashSet<>();
+
+        for (int xInc = -1; xInc <= 1; xInc++) {
+            for (int yInc = -1; yInc <= 1; yInc++) {
+                if (xInc == 0 && yInc == 0) {
+                    continue;
+                }
+                int x = square.getX() + xInc;
+                int y = square.getY() + yInc;
+                if (board.withinBoard(square.getX() + xInc, square.getY() + yInc)) {
+                    Square s = board.getSquareAt(x, y);
+                    if (!opened.contains(s) && isMarked(s)) {
+                        neighbours.add(s);
+                    }
+                }
+            }
+        }
+        return neighbours;
+    }
+
     /**
      * Find out if a square is in the marked set.
-     * 
+     *
      * @param square a Square variable
      * @return if the square is in the marked set.
      */
-    private boolean isFlagged(Square square) {
+    private boolean isMarked(Square square) {
         return marked.contains(square);
     }
 
@@ -197,9 +265,8 @@ public class SecondBot implements Bot {
     }
 
     /**
-     * Find the (X, Y) coordinate pair of an random unopened
-     * square from the current
-     * board
+     * Find the (X, Y) coordinate pair of an random unopened square from the
+     * current board
      *
      * @param board The current board state
      * @return An (X, Y) coordinate pair
@@ -218,7 +285,7 @@ public class SecondBot implements Bot {
         while (!unOpenedSquare) {
             x = rng.nextInt(board.width);
             y = rng.nextInt(board.height);
-            if (!opened.contains(board.board[x][y]) && !isFlagged(board.getSquareAt(x, y))) {
+            if (!opened.contains(board.board[x][y]) && !isMarked(board.getSquareAt(x, y))) {
                 unOpenedSquare = true;
                 pair = new Pair<Integer>(x, y);
             }
@@ -227,4 +294,37 @@ public class SecondBot implements Bot {
         // This pair should point to an unopened square now
         return pair;
     }
+
+    private String squareToString(Square square) {
+        if (square == null) {
+            return "null";
+        }
+        return "(" + square.getX() + "," + square.getY() + ")";
+    }
+
+    /**
+     * Used only for testing/debugging. Converts Square to String
+     * @param s Square variable
+     * @return Square as String
+     */
+    private String squareSetToString(Set<Square> s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+
+        for (Square square : s) {
+            if (sb.length() != 1) {
+                sb.append(", ");
+            }
+            sb.append(squareToString(square));
+
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
+    private Move openMove(Square square) {
+        opened.add(square);
+        return new Move(MoveType.OPEN, square.getX(), square.getY());
+    }
+    
 }
